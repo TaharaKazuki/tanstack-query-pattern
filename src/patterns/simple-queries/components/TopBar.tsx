89b03eb1eb933e81bc382553c9b330da44c781e4
Tahara Kazuki <ssp0727.lnc@gmail.com>
2025-06-28 21:06:07 +0900

    prettier-plugin-tailwindcssを追加し、.prettierrcを更新しました。また、package.jsonとpackage-lock.jsonに依存関係を追加しました。tsconfig.node.jsonに改行を追加しました。

---
+++ src/patterns/simple-queries/components/TopBar.tsx
@@ -0,0 +1,9 @@
+import { Title } from '@mantine/core'
+
+export const TopBar = () => {
+  return (
+    <div className="border-b border-gray-500 p-4">
+      <Title order={1}>Contacts</Title>
+    </div>
+  )
+}
