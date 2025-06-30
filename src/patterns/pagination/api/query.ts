4aeb1d7d1ea71b6c1c8cddcaa79fe785b97ec757
Tahara Kazuki <ssp0727.lnc@gmail.com>
2025-06-30 17:37:50 +0900

    APIクエリのオプションを整理し、クエリキーを定義するためのqueryKeysオブジェクトを追加しました。これにより、コードの可読性と保守性が向上しました。

--- src/patterns/pagination/api/query.ts
+++ src/patterns/pagination/api/query.ts
@@ -2,6 +2,21 @@ import { keepPreviousData, queryOptions } from '@tanstack/react-query'
 
 import { client } from '../../common'
 
+const queryKeys = {
+  all: () => ['contacts'],
+  getContacts: (page: number, count: number) => [
+    ...queryKeys.all(),
+    'list',
+    { page },
+    { count },
+  ],
+  getContact: (contactId: string | undefined) => [
+    ...queryKeys.all(),
+    'one',
+    { contactId },
+  ],
+}
+
 export const getContactsQueryOptions = queryOptions({
   queryKey: ['contacts', 'list'],
   queryFn: () => client.getContacts(),
@@ -9,14 +24,14 @@ export const getContactsQueryOptions = queryOptions({
 
 export const getContactsPaginatedQueryOptions = (page: number, count: number) =>
   queryOptions({
-    queryKey: ['contacts', 'list', { page }, { count }],
+    queryKey: queryKeys.getContacts(page, count),
     queryFn: () => client.getContactsPaginated(page, count),
     placeholderData: keepPreviousData,
   })
 
 export const getOneContactQueryOptions = (contactId?: string) =>
   queryOptions({
-    queryKey: ['contacts', 'one', contactId],
+    queryKey: queryKeys.getContact(contactId),
     queryFn: () => client.getContact(contactId!),
     enabled: contactId !== undefined,
   })
