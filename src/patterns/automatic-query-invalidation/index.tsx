5e10f687d1cd7291908f1735d43f9967829fc121
Tahara Kazuki <ssp0727.lnc@gmail.com>
2025-07-01 16:15:52 +0900

    React Queryを使用して、クエリの成功、エラー、完了時に通知を表示する機能を追加しました。また、QueryProviderをQueryClientProviderに置き換え、クエリの無効化をサポートしました。

--- src/patterns/automatic-query-invalidation/index.tsx
+++ src/patterns/automatic-query-invalidation/index.tsx
@@ -1,11 +1,48 @@
-import { QueryProvider } from '../common'
+import { notifications } from '@mantine/notifications'
+import { IconCircleCheckFilled, IconCircleXFilled } from '@tabler/icons-react'
+import {
+  MutationCache,
+  QueryClient,
+  QueryClientProvider,
+  type QueryKey,
+} from '@tanstack/react-query'
 
 import { ContactsPage } from './components/ContactsPage'
 
+const queryClient = new QueryClient({
+  mutationCache: new MutationCache({
+    onSuccess: (_data, _variables, _context, mutation) => {
+      if (mutation.meta?.successMessage) {
+        notifications.show({
+          icon: <IconCircleCheckFilled />,
+          color: 'green',
+          message: mutation.meta.successMessage as string,
+        })
+      }
+    },
+    onError: (_error, _variables, _context, mutation) => {
+      if (mutation.meta?.errorMessage) {
+        notifications.show({
+          icon: <IconCircleXFilled />,
+          color: 'red',
+          message: mutation.meta.errorMessage as string,
+        })
+      }
+    },
+    onSettled: (_data, _error, _variables, _context, mutation) => {
+      if (mutation.meta?.invalidateQueries) {
+        queryClient.invalidateQueries({
+          queryKey: mutation.meta?.invalidateQueries as QueryKey,
+        })
+      }
+    },
+  }),
+})
+
 export default function Pattern10() {
   return (
-    <QueryProvider>
+    <QueryClientProvider client={queryClient}>
       <ContactsPage />
-    </QueryProvider>
+    </QueryClientProvider>
   )
 }
